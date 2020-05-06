74b1ec3dfbf83a839c42e10dc700b736df6e9ee9
Mihnea <mihn.stn@gmail.com>
2020-05-06 06:33:53 +0300

    Updated example app

--- example/app.d
+++ example/app.d
@@ -15,7 +15,13 @@ void main()
 	env["shape1"] = new Sphere(Vec3(0, 0, -5), 0.5);
 	env["shape2"] = new Sphere(Vec3(-2, 1, -3), 1.5);
 
-	auto cam = new Camera(Vec3.zero, 1920, 1080);
+	auto cam = new CameraBuilder() //
+	.width(1920) //
+	.height(1080) //
+	.fov(10) //
+	.samplesPerPx(100) //
+	.build;
+
 	cam.render(env);
 
 	auto enc = new PPMEncoder();
