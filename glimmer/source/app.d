c0778432dd828a54190b3e1ab6d248b49dd74dec
Mihnea <mihn.stn@gmail.com>
2020-05-08 13:35:49 +0300

    Fixed glass material bug

--- glimmer/source/app.d
+++ glimmer/source/app.d
@@ -16,20 +16,20 @@ void main()
 	// Set shapes
 	env["0"] = new Sphere(Vec3(-1.5, 0, -4), 0.5);
 	env["1"] = new Sphere(Vec3(-.5, 0, -4), 0.5);
-	env["2"] = new Sphere(Vec3(+.5, 0, -4), -0.5);
+	env["2"] = new Sphere(Vec3(+.5, 0, -4), 0.5);
 	env["3"] = new Sphere(Vec3(+1.5, 0, -4), 0.5);
 	env["ground"] = new Sphere(Vec3(0, -100.5, -5), 100);
 
 	// Set materials
 	env["0"] = new Metallic(RGBA.same(0.8), 0.0);
 	env["1"] = new Lambertian(RGBA.opaque(0.6, 0.1, 0.4));
-	env["2"] = new Glass(1.5);
+	env["2"] = new Glass(RGBA.opaque(0.2, 0.5, 0.8), 1.5);
 	env["3"] = new Metallic(RGBA.opaque(1.0, 0.8, 0), 1.0);
 	env["ground"] = new Lambertian(RGBA.opaque(0.2, 0.7, 0.3));
 
 	// Create a new camera at origin
-	auto cam = new CameraBuilder() //.width(1280).height(720)
-	.fov(8).samplesPerPx(100).maxBounces(50).build;
+	auto cam = new CameraBuilder().width(1280).height(720).fov(10)
+		.samplesPerPx(500).maxBounces(75).build;
 
 	// Perform a render of the world
 	cam.render(env);
--- source/glim/materials/glass.d
+++ source/glim/materials/glass.d
@@ -8,38 +8,57 @@ import glim.materials.material;
 ///
 class Glass : Material
 {
+  private RGBA _albedo;
   private double _refractiveIndex;
 
   ///
-  this(double refractiveIndex)
+  this(RGBA albedo, double refractiveIndex)
   {
+    _albedo = albedo;
     _refractiveIndex = refractiveIndex;
+  }
+
+  static private auto schlick(double cosine, double etaQuot)
+  {
+    import std.math : pow;
+
+    auto r0 = pow((1 - etaQuot) / (1 + etaQuot), 2);
+    return r0 + (1 - r0) * pow((1 - cosine), 5);
   }
 
   override bool scatter(const ref Ray ray, const ref Hit hit, out RGBA atten, out Ray bounce) const @safe
   {
     import std.math : sqrt, pow;
     import std.algorithm.comparison : min;
+    import std.random : uniform;
 
-    atten = RGBA.white;
+    atten = _albedo;
 
-    immutable etaQuot = ray.direction.dot(hit.normal) <= 0 //
+    immutable frontHit = ray.direction.dot(hit.normal) <= 0;
+
+    immutable etaQuot = frontHit //
      ? 1.0 / _refractiveIndex //
      : _refractiveIndex //
     ;
 
     immutable dirNorm = ray.direction.normalized;
-    immutable cosTheta = min(1.0, hit.normal.dot(-dirNorm));
+    immutable rayFacingNorm = frontHit ? hit.normal : -hit.normal;
+
+    immutable cosTheta = min(1.0, rayFacingNorm.dot(-dirNorm));
     immutable sinTheta = sqrt(1.0 - pow(cosTheta, 2));
 
-    if (etaQuot * sinTheta > 1.0)
+    immutable shouldReflect = etaQuot * sinTheta > 1.0 //
+     || uniform(0.0, 1.0) < schlick(cosTheta, etaQuot) //
+    ;
+
+    if (shouldReflect)
     {
-      immutable refl = dirNorm.reflect(hit.normal);
+      immutable refl = dirNorm.reflect(rayFacingNorm);
       bounce = Ray(hit.position, refl);
     }
     else
     {
-      immutable refr = dirNorm.refract(hit.normal, etaQuot);
+      immutable refr = dirNorm.refract(rayFacingNorm, etaQuot);
       bounce = Ray(hit.position, refr);
     }
 
